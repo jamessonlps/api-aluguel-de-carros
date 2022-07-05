f31ef6bdea7bcdaa287634ca7d547ac42c537485
jamessonlps <jamesson1233@gmail.com>
2022-07-05 11:17:59 -0300

    inicio da documentacao

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import { router } from './routes';
+import SwaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(SwaggerFile));
 app.use(router);
 
 app.listen(3333, () => {
