ffc53d4c7b82c1518ebe063b37fa16928f3a4a0d
jamessonlps <jamesson1233@gmail.com>
2022-07-06 14:18:05 -0300

    add typeorm and docker

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -4,10 +4,10 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    this.createCategoryUseCase.execute({ description, name });
+    await this.createCategoryUseCase.execute({ description, name });
 
     return response.status(201).send();
   }
